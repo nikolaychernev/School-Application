e1feca7d12978f0f3a1ac269d356e906c24d6bc7
Nikolay Chernev <nikolay.chernev@dreamix.eu>
2022-07-12 13:45:39 +0300

    Lecture 75

---
+++ ViewController/src/view/bean/StudentsBean.java
@@ -0,0 +1,16 @@
+package view.bean;
+
+import oracle.adf.view.rich.event.DialogEvent;
+
+import view.common.MyADFUtil;
+
+public class StudentsBean {
+    public StudentsBean() {
+    }
+
+    public void deleteStudentDialogListener(DialogEvent dialogEvent) {
+        MyADFUtil.executeOperation("Delete");
+        MyADFUtil.executeOperation("Commit");
+        MyADFUtil.showSuccessfulMessage("Student deleted successfully.");
+    }
+}
