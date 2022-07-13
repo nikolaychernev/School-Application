e0c4e3ca4957127cd6b8d8fc41c1bf4b435f082b
Nikolay Chernev <nikolay.chernev@dreamix.eu>
2022-07-13 14:09:08 +0300

    Lecture 81

--- Model/src/model/common/ModelUtil.java
+++ Model/src/model/common/ModelUtil.java
@@ -2,6 +2,8 @@ package model.common;
 
 import java.math.BigDecimal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 import java.util.Date;
@@ -19,5 +21,29 @@ public class ModelUtil {
     public static Timestamp getCurrentDate() {
         Date today = new Date();
         return new Timestamp(today.getTime());
+    }
+    
+    public static String getSqlDescription(String sql, DBTransaction trans) {
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        
+        try {
+            stat = trans.createPreparedStatement(sql, 1);
+            rs = stat.executeQuery();
+            
+            while (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                stat.close();
+            } catch (Exception e) {
+            }
+        }
+        
+        return "";
     }
 }
